78b8216c9ca2affbdb129ed8959689f961a519d9
Luigi Lupini <luigi.lupini@gmail.com>
2022-10-14 14:44:40 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+function App() {
+  return <div className="App">Our motion playground is ready 🤘</div>;
+}
+
+export default App;
