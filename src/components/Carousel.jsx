aafb25be5830913293b6ac45397b0bf21341b95c
Luigi Lupini <luigi.lupini@gmail.com>
2022-10-15 07:30:56 +0200

    6) Basic mapping over images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,12 +1,15 @@
 import "./Carousel.css";
 import { useRef, useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import images from "../utils/images";
 
 export default function Carousel() {
   return (
     <motion.div className="carousel">
       <motion.div className="inner-carousel">
-        <h1>Starting a carousel</h1>
+        {images.map((image) => (
+          <img src={image} className="item" />
+        ))}
       </motion.div>
     </motion.div>
   );
---
+++ src/utils/images.js
@@ -0,0 +1,27 @@
+import image1 from "../assets/pexels-ashley-fontana-705782.jpg";
+import image2 from "../assets/pexels-cmonphotography-1809640.jpg";
+import image3 from "../assets/pexels-davide-cacciatori-3762321.jpg";
+import image4 from "../assets/pexels-edgar-colomba-5122256.jpg";
+import image5 from "../assets/pexels-fineas-anton-2993065.jpg";
+import image6 from "../assets/pexels-josh-hild-2422259.jpg";
+import image7 from "../assets/pexels-ledana-mance-11265951.jpg";
+import image8 from "../assets/pexels-maegan-white-981682.jpg";
+import image9 from "../assets/pexels-matt-hardy-3560024.jpg";
+import image10 from "../assets/pexels-melissa-thomas-1498843.jpg";
+import image11 from "../assets/pexels-michael-block-3225528.jpg";
+import image12 from "../assets/pexels-vincent-rivaud-2233348.jpg";
+
+export default [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+];
